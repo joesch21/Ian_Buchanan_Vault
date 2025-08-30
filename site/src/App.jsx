a2244af878eb57cbd7d4be0746082dd01067a3b9
joesch21 <132034665+joesch21@users.noreply.github.com>
2025-08-30 14:01:32 +1000

    feat: add bibliography page

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,16 +1,19 @@
 import { Routes, Route, Link } from 'react-router-dom'
+import Bibliography from './pages/Bibliography.jsx'
 
 export default function App() {
   return (
     <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
       <h1>Ian Buchanan Vault</h1>
       <nav>
-        <Link to="/">Home</Link> |{" "}
+        <Link to="/">Home</Link> |{' '}
+        <Link to="/bibliography">Bibliography</Link> |{' '}
         <Link to="/about">About</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/bibliography" element={<Bibliography />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
